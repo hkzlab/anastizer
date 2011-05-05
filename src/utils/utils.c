59dc0e13bfd43594c2cd4f0b6c513d98f02ef240
Fabio Battaglia <hkzlabnet@gmail.com>
2011-05-05 18:32:29 +0200

    Additional tuning

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -109,6 +109,8 @@ IplImage *anastize_image(IplImage *wimg, Uint8 cur_chan) {
 
 	IplImage *mimg = cvCreateImage(cvGetSize(wimg), 8, 1);	
 
+	cvSmooth(wimg, wimg, CV_BLUR, 19, 0, 0, 0);
+
 	fprintf(stdout, " Applying local thresholding to image...\n");
 	cvAdaptiveThreshold(wimg, mimg, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, tmask_size, tmask_avr);
 
@@ -120,6 +122,12 @@ IplImage *anastize_image(IplImage *wimg, Uint8 cur_chan) {
 	remove_spot_neighbour_dist(mimg, 4 * WARP_MULT + 1, 8 * WARP_MULT, 8 * WARP_MULT, Conn8); 
 	remove_spot_neighbour_dist(mimg, 8 * WARP_MULT + 1, 150 * WARP_MULT, 30 * WARP_MULT, Conn8);
 	remove_spot_thin(mimg, 1, 5 * WARP_MULT, 0.6, Conn8); // Do a cleanup based on thinness of the element
+	
+	// This code might be useful for segmentation: a way to find where main text really is
+	/*
+	cvThreshold(wimg, mimg, 200, 255, CV_THRESH_BINARY);
+	cvErode(mimg, mimg, NULL, 60);
+	*/
 
 	return mimg;
 }
