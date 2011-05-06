dca45de967b4b23bdb11c3c7ab99285202d09689
Fabio Battaglia <hkzlabnet@gmail.com>
2011-05-06 19:11:47 +0200

    Add additional test code to bookdet

--- src/bookdet.c
+++ src/bookdet.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
 	Uint32 nwidth, nheight;
-	IplImage *oimg, *tmp_img;
+	IplImage *oimg, *tmp_img, *smimg;
 
 	// Check parameters and load image file
 	if (argc < 2) {
@@ -19,13 +19,26 @@ int main(int argc, char *argv[]) {
 		fprintf(stdout, "Loaded image %s\n", argv[1]);
 	}
 
+	nwidth = oimg->width;
+	nheight = oimg->height;
 	recalc_img_size(&nwidth, &nheight, ORIG_H);
 	tmp_img = cvCreateImage(cvSize(nwidth , nheight), oimg->depth, oimg->nChannels);
 	cvResize(oimg, tmp_img, CV_INTER_CUBIC);
-
-	cvReleaseImage(&tmp_img);
 	cvReleaseImage(&oimg);
+	oimg = tmp_img; // Replace original image with the resized version
 
+	nwidth = oimg->width;
+	nheight = oimg->height;
+	recalc_img_size(&nwidth, &nheight, 256);
+	smimg = cvCreateImage(cvSize(nwidth , nheight), oimg->depth, oimg->nChannels);
+	cvResize(oimg, smimg, CV_INTER_NN); // Create a very small preview image
+
+	cvSmooth(smimg, smimg, CV_BLUR, 25, 0, 0, 0);
+	cvThreshold(smimg, smimg, 190, 255, CV_THRESH_BINARY_INV);
+	cvSaveImage("./test.jpg", smimg, 0);
+
+	cvReleaseImage(&smimg);
+	cvReleaseImage(&oimg);
 	return 0;
 }
 
