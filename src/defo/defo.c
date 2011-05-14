ecb5da21dc263b1d8ca1baf48c860fc86bc726c3
Fabio Battaglia <hkzlabnet@gmail.com>
2011-05-14 12:24:49 +0200

    Add some utility functions

--- src/defo/defo.c
+++ src/defo/defo.c
@@ -1,1 +1,23 @@
 #include "defo.h"
+
+defo_grid *allocDefoGrid(Uint32 width, Uint32 height) {
+	assert(width && height);
+
+	defo_grid *d = (defo_grid*) malloc(sizeof(defo_grid));
+	d->width = width;
+	d->height = height;
+
+	d->pnt = (CvPoint2D32f*)malloc(sizeof(CvPoint2D32f) * width * height);
+
+	return d;
+}
+
+void *freeDefoGrid(defo_grid **dg) {
+	assert(*dg);
+
+	free((*dg)->pnt);
+	free(*dg);
+
+	*dg = NULL;
+}
+
--- src/defo/defo.h
+++ src/defo/defo.h
@@ -3,4 +3,13 @@
 
 #include "common/defs.h"
 
+typedef struct {
+	Uint32 width;
+	Uint32 height;
+	CvPoint2D32f *pnt;
+} defo_grid;
+
+defo_grid *allocDefoGrid(Uint32 width, Uint32 height);
+void *freeDefoGrid(defo_grid **dg);
+
 #endif /* _DEFO_HEADER_ */
